4849a24dcd37125e8e2d0cd7b2d41c8577d8cbdc
Jeffrey Brown <jeffrey@mcgaw.io>
2023-11-21 14:27:46 -0500

    pathway generator basic functionality

--- src/lib/helpers/pathwayRenderer.js
+++ src/lib/helpers/pathwayRenderer.js
@@ -1,16 +1,20 @@
 // define constants //
+/* <rect x="556.35" y="1440.35" width="136.3" height="45.3" rx="2" fill="rgb(255,229,153)" fill-opacity="1" stroke="rgb(0,0,0)" stroke-width="2.7"></rect>
+<text x="594" y="1468.6370000839233" fill="rgb(0,0,0)" font-style="normal" font-weight="normal" font-size="17px"><tspan>Tailwind</tspan></text> */
+
 // ... //
 
-// process colors //
-function convertColors(code) {
-  let r = code >> 16;
-  let g = code >> 8 & 255;
-  let b = code & 255
-  return `rgb(${r},${g},${b})`;
-}
+// // process colors //
+// function convertColors(code) {
+//   let r = code >> 16;
+//   let g = code >> 8 & 255;
+//   let b = code & 255
+//   return `rgb(${r},${g},${b})`;
+// }
 
-// create page and document elements //
-function createSVGElement(element, attributes = {}, parent ) {
+// // create page and document elements //
+async function createVectorElement(element, attributes = {}, parent) {
+  console.log("...creating element...", element, attributes, parent)
 	let node = document.createElementNS("http://www.w3.org/2000/svg", element); 
 	for (let attribute in attributes) {
 		attributes.hasOwnProperty(attribute) && node.setAttribute(attribute, attributes[attribute])
@@ -18,13 +22,78 @@ function createSVGElement(element, attributes = {}, parent ) {
 	return parent && parent.appendChild(node), node;
 }
 
-class Renderer {
-	constructor(svgRoot, fontFamily) {
-		this.svgRoot = svgRoot, this.fontFamily = fontFamily;
-		this.canvasRenderingContext2D = document.createElement("canvas").getContext("2d");
-  }
+// create shape: rectangle //
+function createRectangle(node, parent) {
+  console.log("...creating rectangle...");
+
+  // create group container //
+  let group = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+  group.setAttribute("class", "clickable-group");
+  group.setAttribute("data-group-id", node.nodeTitle );
+
+  // create rectangle vector //
+
+  let rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+  rect.setAttribute("x", node.xpos);
+  rect.setAttribute("y", node.ypos);
+  rect.setAttribute("width", node.width);
+  rect.setAttribute("height", node.height);
+  rect.setAttribute("rx", node.properties.radius);
+  rect.setAttribute("fill", node.properties.bgColor);
+  rect.setAttribute("fill-opacity", node.properties.opacity);
+  rect.setAttribute("stroke", node.properties.bdColor);
+  rect.setAttribute("stroke-width", node.properties.bdWidth);
+
+  parent.appendChild(group);
+  group.appendChild(rect);
 }
 
-export async function renderPolymathPathway(data, attributes = {}) {
-  console.log('hi')
+function renderLinkIcon(node) {
+  const iconSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  const iconPath = document.createElementNS(
+    'http://www.w3.org/2000/svg',
+    'path'
+  );
+
+  iconSvg.setAttribute('fill', 'none');
+  iconSvg.setAttribute('viewBox', '0 0 24 24');
+  iconSvg.setAttribute('stroke', 'black');
+  iconSvg.classList.add('post-icon');
+
+  iconPath.setAttribute(
+    'd',
+    'M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1'
+  );
+  iconPath.setAttribute('stroke-linecap', 'round');
+  iconPath.setAttribute('stroke-linejoin', 'round');
+  iconPath.setAttribute('stroke-width', '2');
+
+  iconSvg.appendChild(iconPath);
+
+  return node.appendChild(iconSvg);
+}
+
+
+// main function //
+export async function renderPolymathPathway(data, pathwayRoot) {
+  console.log("...rendering pathway...")
+
+  // // define key variables //
+  const nodes = data.nodes;
+  const configuration = data.meta.configuration;
+
+  // // create the primary pathway frame //
+  const pathwayFrame = await createVectorElement("svg", { 
+    "xmlns": "http://www.w3.org/2000/svg",
+    "xmlns:xlink": "http://www.w3.org/1999/xlink",
+    "viewBox": `${configuration.minw}, ${configuration.minh}, ${configuration.width}, ${configuration.height}` 
+  }, pathwayRoot);
+
+  // loop through nodes and render elements //
+  for (let node in nodes) {
+    console.log("...generating node: ", nodes[node]);
+    if (nodes[node].nodeTypeId == 'rect') {
+      createRectangle(nodes[node], pathwayFrame)
+    }
+  }
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,50 +1,48 @@
 <script>
 	import { onMount } from "svelte";
-	import Token  from "../lib/components/tokens/Token.svelte";
-	import { tokens as staticTokens } from "../assets/data/tokens";
 	import { renderPolymathPathway } from "../lib/helpers/pathwayRenderer";
-
-
-	const testPathway = {
-  nodes: [
-    {
-      id: "1",
-      nodeTypeId: "rectSm",
-      width: "142",
-      height: "44",
-      xpos: "0",
-      ypos: "0",
-    }
-  ],
-  meta: {
-    attributes: {
-      name: "Front-End Web Development"
-    }
-  }
-}
-
-	renderPolymathPathway()
-
-	let isLoading = true;
-	let htmlTokens;
-
-	const getTokens = async() => {
-		// let tokenFetch = await fetch('./assets/data/tokens.json');
-		// let tokens = await tokenFetch.json();
-		return staticTokens;
-	}
-
-	const loadTokens = async() => {
-		return getTokens().then((tokens) => {
-			window.tokens = tokens;
-			htmlTokens = tokens.slice(0,1);
-
-			isLoading = false;
-		});
+	const pathwayData = {
+		nodes: [
+			{
+				id: "1",
+				nodeTitle: "the-internet",
+				nodeTypeId: "rect",
+				width: "200",
+				height: "80",
+				xpos: "10",
+				ypos: "35",
+				properties: {
+					bgColor: "rgb(255,255,255)",
+					bdColor: "rgb(0,0,0)",
+					bdWidth: "2",
+					radius: "1",
+					opacity: "1"
+				},
+				attributes: {
+					
+				},
+				label: {
+					text: "Testing"
+				}
+			}
+		],
+		meta: {
+			attributes: {
+				name: "Front-End Web Development"
+			},
+			configuration: {
+				minw: "0",
+				minh: "0",
+				width: "1280", 
+				height: "1000"
+			}
+		}
 	}
 
 	onMount(() => {
-		loadTokens();
+		const pathwayRoot = document.getElementById("polymath-pathway-wrapper");
+		console.log(pathwayData, pathwayRoot)
+		renderPolymathPathway(pathwayData, pathwayRoot);
 	});
 </script>
 
@@ -62,7 +60,7 @@
 	<main>
 		<div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
 			<div class="px-4 py-8 sm:px-0">
-				<div class="h-96 rounded-md border border-gray-200 bg-white p-2.5 hover:border-gray-400 sm:rounded-lg sm:p-5">
+				<div class="rounded-md border border-gray-200 bg-white p-2.5 hover:border-gray-400 sm:rounded-lg sm:p-5">
 					<div class="relative border-b border-gray-200 pb-5 sm:pb-0">
 						<div class="md:flex md:items-center md:justify-between">
 							<h3 class="text-lg font-medium leading-6 text-gray-900">Pathways</h3>
@@ -81,50 +79,17 @@
 							</div>
 							<div class="hidden sm:block">
 								<nav class="-mb-px flex space-x-8">
-									<a href="/" class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap pb-4 px-1 border-b-2 font-medium text-sm">Front-End Development</a>
-									<a href="/" class="border-indigo-500 text-indigo-600 whitespace-nowrap pb-4 px-1 border-b-2 font-medium text-sm" aria-current="page">Back-End Development</a>
+									<a href="/" class="border-indigo-500 text-indigo-600 whitespace-nowrap pb-4 px-1 border-b-2 font-medium text-sm">Front-End Development</a>
+									<a href="/" class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap pb-4 px-1 border-b-2 font-medium text-sm">Back-End Development</a>
 								</nav>
 							</div>
 						</div> 
 					</div>
-					<div id="resource-svg-wrap" style="" data-resource-type="roadmap" data-resource-id="frontend">
-						<!-- <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="143 186 1230 3288" style="font-family: balsamiq">
-							<path d="M775 376Q774.9873005977104 417.9762280494307 775 470.5703621905065" fill="none" stroke="rgb(43,120,228)" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="undefined"></path>
-							<g class="clickable-group" data-group-id="100-internet">
-								<rect x="500" y="200" width="172.3" height="47.3" rx="10" fill="rgb(0,255,0)" fill-opacity="1" stroke="rgb(0,20,30)" stroke-width="2"></rect>
-								<text x="744" y="483.5859999656677" fill="rgb(0,0,0)" font-style="normal" font-weight="normal" font-size="17px">
-									<tspan>Internet</tspan>
-								</text>
-							</g>
-						</svg> -->
+					<div id="polymath-pathway-wrapper" style="" class="--aspect-ratio" data-resource-type="roadmap" data-resource-id="frontend">
+
 					</div>
 				</div>
 			</div>
 		</div>
 	</main>
 </div>
-
-<!-- <div class="py-6">
-	<div class="mx-auto px-4 sm:px-6 md:px-8">
-		<div class="py-4">
-			<div class="h-96">
-				{/if isLoading}
-				loading...
-				{:else}
-					{/each htmlTokens as token, index}
-						<Token
-							id = {token.id}
-							title = {token.title}
-							images = {token.images}
-							description = {token.description}
-							category = {token.category}
-							subcategory = {token.subcategory}
-							number = {token.number}
-							difficulty = {token.difficulty}
-						/>
-					{/each}
-				{/if}
-			</div>
-		</div>
-	</div>
-</div> -->
